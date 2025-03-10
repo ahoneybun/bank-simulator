82422756310d3b3755c31f7c02af3ab11f6b8529
Aaron Honeycutt <aaronhoneycutt@proton.me>
2025-03-10 10:12:59 -0600

    using stdin to set the pin

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,54 @@
-// use std::io;
+use std::io;
 
 fn welcome_message() {
-    println!("Welcome to Thelio Bank Inc!")
+    println!("Welcome to Thelio Bank Inc!");
+    println!();
 }
 
 fn main() {
     println!();
     welcome_message();
 
-    let mut account = BankAccount {
+    let account = BankAccount {
         owner: "Aaron Honeycutt".to_string(),
+        account_number: 123456789,
         balance: 145.50,
-        account_number: 1234
+        pin: 1234,
+
     };
    
     println!("Welcome {}", account.owner);
-    account.check_balance();
+    println!();
+
+    println!("What would you like to set your PIN to?");
+    account.set_pin();
+    println!();
 }
 
 struct BankAccount {
     owner: String,
     account_number: i128,
     balance: f64,
+    pin: u16,
 }
 
 impl BankAccount {
-    fn desposit(&mut self, amount: f64) {
-        println!("Depositing {} to account ending in {}", amount, self.account_number);
-        self.balance += amount;
-    }
+    // fn desposit(&mut self, amount: f64) {
+    //     println!("Depositing {} to account ending in {}", amount, self.account_number);
+    //     self.balance += amount;
+    // }
 
     fn check_balance(&self) {
         println!("your current balance is {} in account ending in {}", self.balance, self.account_number);
+    }
+
+    fn set_pin(&self) {
+        let mut pin = String::new();
+
+        io::stdin()
+            .read_line(&mut pin)
+            .expect("Failed to read line");
+    
+        println!("You have set your pin successfully.");
     }
 }
